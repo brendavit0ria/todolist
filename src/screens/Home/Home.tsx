f762d64cf6ec53d838b6426cac4dbe125de62f17
brendavit0ria <brendavitoria0602@gmail.com>
2025-04-15 16:27:58 -0300

    feat: new task form

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,25 @@
-import { View, Image } from "react-native";
+import { View, Image, TextInput, TouchableOpacity, Text } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
   return (
     <View>
       <View style={styles.container}>
         <Image source={require("../../assets/logo.png")} />
       </View>
-      <View style={styles.content}></View>
+
+      <View style={styles.content}>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor="#808080"
+          />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: 210,
+    height: 200,
     backgroundColor: "#0D0D0D",
     justifyContent: "center",
     alignItems: "center",
@@ -11,5 +11,33 @@ export const styles = StyleSheet.create({
   content: {
     height: "100%",
     backgroundColor: "#1A1A1A",
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: "#262626",
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    fontSize: 18,
+    marginRight: 12,
+  },
+  form: {
+    flexDirection: "row",
+    marginHorizontal: 24,
+    marginTop: -30,
+    marginBottom: 42,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#1E6F9F",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#fff",
   },
 });
